66b6e00672a2394dd65b66297ff1b7328b7ef91e
BrayamMendez <117295357+bkBrayam@users.noreply.github.com>
2023-10-27 09:58:53 -0600

    post Task

--- src/main/java/com/example/crud02/controller/TaskController.java
+++ src/main/java/com/example/crud02/controller/TaskController.java
@@ -4,22 +4,25 @@ import com.example.crud02.dto.TaskRequest;
 import com.example.crud02.entity.Task;
 import com.example.crud02.service.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/tasks")
+@RequestMapping(path = "api/v1")
 @AllArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
 
-    @GetMapping
+    @GetMapping(value = "tasks")
     public List<Task> getTask() {
         return taskService.getTasks();
+    }
+
+    @PostMapping(value = "task")
+    public ResponseEntity<Object> postTask(@RequestBody TaskRequest request){
+        return taskService.postTask(request);
     }
 }
--- src/main/java/com/example/crud02/service/TaskService.java
+++ src/main/java/com/example/crud02/service/TaskService.java
@@ -1,10 +1,13 @@
 package com.example.crud02.service;
 
+import com.example.crud02.dto.TaskRequest;
 import com.example.crud02.entity.Task;
 import com.example.crud02.repository.TaskRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,5 +17,17 @@ public class TaskService {
     private final TaskRepository taskRepository;
     public List<Task> getTasks() {
         return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Object> postTask(TaskRequest request) {
+        Task task = Task.builder()
+                .task(request.getTask())
+                .description(request.getDescription())
+                .status(request.getStatus())
+                .initialDate(new Date())
+                .build();
+
+        taskRepository.save(task);
+        return ResponseEntity.ok(task);
     }
 }
